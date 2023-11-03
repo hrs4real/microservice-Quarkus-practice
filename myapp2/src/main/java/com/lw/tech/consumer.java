243bef7c35c4519453029fe89955a4c8478a2662
Harsh Srivastava <hsrivastava130@gmail.com>
2023-11-04 02:35:37 +0530

    myapp2 updated

--- myapp2/src/main/java/com/lw/tech/consumer.java
+++ myapp2/src/main/java/com/lw/tech/consumer.java
@@ -2,10 +2,13 @@ package com.lw.tech;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.Optional;
+
 @Path("/consumer")
 public class consumer {
 
@@ -13,11 +16,24 @@ public class consumer {
     @Path("/all")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Student> getAllStudents(){
+    public List<Student> getAllStudents() {
         return List.of(
-                new Student(1,"Jack k",1111,"java"),
-                new Student(2,"Henry",2222,"cpp"),
-                new Student(3,"Andrew",3333,"python")
+                new Student(1, "Jack k", 1111, "java"),
+                new Student(2, "Henry", 2222, "cpp"),
+                new Student(3, "Andrew", 3333, "python")
         );
+    }
+
+        @Path("/count")
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public int countAllStudents(){
+            return getAllStudents().size();
+    }
+    @Path("{x}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<Student> getStudent(@PathParam("x") int id){
+        return getAllStudents().stream().filter(Student -> Student.id == id).findFirst();
     }
 }
